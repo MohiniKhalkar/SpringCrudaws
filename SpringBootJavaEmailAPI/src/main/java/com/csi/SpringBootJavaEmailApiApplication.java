aaf330c096c7cc4a32313ef54eb4088185bfca04
MohiniKhalkar <khalkarmohini123@gmail.com>
2022-09-02 23:49:36 +0530

    commit by mohini k

---
+++ SpringBootJavaEmailAPI/src/main/java/com/csi/SpringBootJavaEmailApiApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaEmailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJavaEmailApiApplication.class, args);
+	}
+
+}
